d3f291d9371fbd0a008465e6dd1ab7fca9025144
杨康 <8757583+yang-kangyy@user.noreply.gitee.com>
2021-09-04 14:12:56 +0800

    项目初始化，调整目录结构

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+// 加载全局样式文件
+import './styles/main.less'
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
