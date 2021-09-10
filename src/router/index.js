5e6c91d60308d2d70c5bfe351765f41762cb1b63
杨康 <8757583+yang-kangyy@user.noreply.gitee.com>
2021-09-10 15:00:57 +0800

    完成了内容管理模块的开发

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/views/login'
+import Home from '@/views/home'
+import Layout from '@/views/layout'
+import Article from '@/views/article'
 
 Vue.use(VueRouter)
 
@@ -10,12 +13,64 @@ const routes = [
     path: '/login',
     name: 'login',
     component: Login
-  }
+  },
+  {
+    path: '/',
+    // 命名路由 layout 有一个默认子路由，这个名字没有意义，所以警告
+    // 正确的做法是：如果有默认子路由，就不要给父路由起名字了
+    // name: 'layout',
+    component: Layout,
+    children: [
+      {
+        // 我们通常会把根路径 / 设置为网站的首页
+        // 为啥呢？因为我们在手动输入网址访问网站的时候，可以省略 /
+        // 如果你是别的名称，则必须加上
+        path: '', // path 为空，会作为默认子路由渲染
 
+        // 路由的名字是干啥的？
+        // 参考：https://gitee.com/lipengzhou/toutiao-publish-admin/issues/I1F1BA
+        name: 'home',
+        component: Home
+      },
+      {
+        path: '/article',
+        name: 'article',
+        component: Article
+      }
+    ]
+  }
 ]
 
 const router = new VueRouter({
   routes
+})
+// 路由导航守卫：说白了所有页面的导航都会经过这里
+// 守卫页面的导航的
+// to：要去的路由信息
+// from：来自哪里的路由信息
+// next：放行方法
+router.beforeEach((to, from, next) => {
+  // 如果要访问的页面不是 /login，校验登录状态
+  // 如果没有登录，则跳转到登录页面
+  // 如果登录了，则允许通过
+  // 允许通过
+  // next()
+
+  const user = JSON.parse(window.localStorage.getItem('user'))
+
+  // 校验非登录页面的登录状态
+  if (to.path !== '/login') {
+    if (user) {
+      // 已登录，允许通过
+      next()
+    } else {
+      // 没有登录，跳转到登录页面
+      next('/login')
+    }
+  } else {
+    // 登录页面，正常允许通过
+    next()
+  }
 })
 
 export default router
---
+++ src/views/layout/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-container class="layout-container">
+    <el-aside class="aside" width="auto">
+      <app-aside class="aside-menu"></app-aside>
+    </el-aside>
+    <el-container>
+      <el-header class="header">
+        <app-header></app-header>
+      </el-header>
+      <el-main class="main">
+        <!-- 子路由出口 -->
+        <router-view />
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import AppAside from './components/aside'
+import AppHeader from './components/header'
+export default {
+  name: 'LayoutIndex',
+  components: { AppAside, AppHeader },
+  props: {},
+  data () {
+    return {
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {
+
+  },
+  mounted () { },
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang="less">
+.layout-container {
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+}
+
+.aside {
+  background-color: #d3dce6;
+  .aside-menu {
+    height: 100%;
+  }
+}
+
+.main {
+  background-color: #e9eef3;
+}
+</style>
